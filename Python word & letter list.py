478db2629936210c125e8c947103124fc02e41ed
Garry decake <66247397+abhinavkatore@users.noreply.github.com>
2020-10-03 11:21:21 +0530

    Python word magic

---
+++ Python word & letter list.py
@@ -0,0 +1,34 @@
+words = ["Hello", "world", "!"]
+x=('Given Statement:words = ["Hello", "world", "!"]')
+print(x)
+print('print(words[0]):',words[0])
+
+#But if --->
+
+print('print(words [0][1]):', words [0][1])     
+#or
+print('print(words [0][0]): ',words [0][0]) 
+print('print('  'words[1]):',words[1])
+print('print(words[2]) :',words[2])
+print(words[0:4])
+letter= ('ABCDEFG')
+print(letter[0])
+print(letter[1:])
+
+
+x=('ABCDEFGH')
+print(x[0:])
+print(x[:])
+print(x[1:]) 
+print(x[1:7])
+
+x='python_is_coding_language'
+y=(x[0:])
+a=input()
+z=(y.find(a))
+print('position:',z) 
+print('your_letter:',a )
+
+
+
+   # this code can find your letter or words in the following sentence👊🏻👊🏻
